ff56d45f84c856b50c8404d184fea15c12454503
EnglerJan <hockye@seznam.cz>
2025-08-02 08:05:59 +0200

    Add requirements.txt file, logging

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 Pro zprovozneni na svem pc je potreba sehnat si discord api token a vytvorit .env a dat do nej: 
 DISCORD_TOKEN = -token-
 najdete ten token na https://discord.com/developers/applications
+
+Potrebne python knihovny:
+pip install -r requirements.txt
+
 Vhodny pouze pro jeden server!!
+
 Autori: Jaromir Kligl, Jan Engler
+
+
--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from discord.ext import commands
 from discord import Intents
 from dotenv import load_dotenv
 import pathlib
+import logging
 
 import games
 import images
 import songs
 import gamba
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,6 +23,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Setup intents
 intents = Intents.default()
 intents.message_content = True
+intents.voice_states = True
 
 # Bot instance
 bot = commands.Bot(command_prefix='%', intents=intents)
